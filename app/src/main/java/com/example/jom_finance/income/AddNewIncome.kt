1d7901e1f8d8bae9ba17ef1f98712a74612d9903
cheah wooi kang <cheahwooikang02@gmail.com>
2021-10-15 16:55:33 +0800

    add AddNewIncome.kt and incomeFragment.kt

---
+++ app/src/main/java/com/example/jom_finance/income/AddNewIncome.kt
@@ -0,0 +1,13 @@
+package com.example.jom_finance.income
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.jom_finance.R
+
+class AddNewIncome : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_new_income)
+
+    }
+}
---
+++ app/src/main/java/com/example/jom_finance/income/incomeFragment.kt
@@ -0,0 +1,80 @@
+package com.example.jom_finance.income
+
+import android.content.Context
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.jom_finance.R
+import kotlinx.android.synthetic.main.fragment_income.*
+import android.widget.ArrayAdapter
+
+
+
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [incomeFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class incomeFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_income, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        ArrayAdapter.createFromResource(
+            activity as Context,
+            R.array.Category,
+            android.R.layout.simple_spinner_item).also { adapter ->
+            // Specify the layout to use when the list of choices appears
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            // Apply the adapter to the spinner
+            spinnerCategory.adapter = adapter
+        }
+
+
+    }
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment incomeFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            incomeFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
+}
