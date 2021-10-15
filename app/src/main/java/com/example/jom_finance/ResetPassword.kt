179359c96851c2cfea0218a38b960853068c9318
cheah wooi kang <cheahwooikang02@gmail.com>
2021-10-15 12:34:51 +0800

    add ResetPassword activity

--- app/src/main/java/com/example/jom_finance/ResetPassword.kt
+++ app/src/main/java/com/example/jom_finance/ResetPassword.kt
@@ -7,5 +7,6 @@ class ResetPassword : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_reset_password)
+
     }
 }
