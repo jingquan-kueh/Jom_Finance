7c67843a7690f4a2febf6c6cc5a554b90e12fb64
jing quan <jingquan127@gmail.com>
2021-11-07 05:13:43 +0800

    Commit by JQ

--- app/src/main/java/com/example/jom_finance/AccountsListActivity.kt
+++ app/src/main/java/com/example/jom_finance/AccountsListActivity.kt
@@ -3,10 +3,26 @@ package com.example.jom_finance
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.jom_finance.setting.CurrencyActivity
+import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.jom_finance.databinding.AccountListAdapter
+import com.example.jom_finance.models.Account
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.*
 import kotlinx.android.synthetic.main.activity_accounts_list.*
 
+
 class AccountsListActivity : AppCompatActivity() {
+
+    private lateinit var fAuth : FirebaseAuth
+    private lateinit var db : FirebaseFirestore
+    private lateinit var userID : String
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var accountArrayList : ArrayList<Account>
+    private lateinit var accountListAdapter : AccountListAdapter
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_accounts_list)
@@ -21,7 +37,43 @@ class AccountsListActivity : AppCompatActivity() {
             overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left)
         }
 
+        setUpdb()
+        recyclerView = accounts_recyclerView
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.isNestedScrollingEnabled = true
+        recyclerView.setHasFixedSize(true)
+        accountArrayList= arrayListOf()
+        accountListAdapter = AccountListAdapter(accountArrayList)
+        recyclerView.adapter = accountListAdapter
+        EventChangeListener()
 
+    }
+
+    private fun EventChangeListener() {
+        db = FirebaseFirestore.getInstance()
+        db.collection("accounts/$userID/account_detail")
+            .addSnapshotListener(object : EventListener<QuerySnapshot> {
+                override fun onEvent(value: QuerySnapshot?, error: FirebaseFirestoreException?) {
+                    if(error!=null){
+                        Log.e("FireStore Error",error.message.toString())
+                        return
+                    }
+                    for(dc : DocumentChange in value?.documentChanges!!){
+                        if(dc.type == DocumentChange.Type.ADDED){
+                            accountArrayList.add(dc.document.toObject(Account::class.java))
+                        }
+                    }
+                    accountListAdapter.notifyDataSetChanged()
+                }
+            })
+    }
+
+    private fun setUpdb(){
+        fAuth = FirebaseAuth.getInstance()
+        val currentUser = fAuth.currentUser
+        if (currentUser != null) {
+            userID = currentUser.uid
+        }
     }
 
     override fun onBackPressed() {
--- app/src/main/java/com/example/jom_finance/databinding/AccountListAdapter.kt
+++ app/src/main/java/com/example/jom_finance/databinding/AccountListAdapter.kt
@@ -1,4 +1,50 @@
 package com.example.jom_finance.databinding
 
-class AccountListAdapter {
+import android.graphics.Color.WHITE
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.jom_finance.R
+import com.example.jom_finance.models.Account
+import com.maltaisn.icondialog.pack.IconDrawableLoader
+import com.maltaisn.icondialog.pack.IconPackLoader
+import com.maltaisn.iconpack.defaultpack.createDefaultIconPack
+
+class AccountListAdapter(private val accountList : ArrayList<Account>) : RecyclerView.Adapter<AccountListAdapter.ListViewHolder>(){
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListViewHolder {
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.item_account,parent,false)
+        return ListViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
+        val account : Account = accountList[position]
+        holder.title.text = account.accountName.toString()
+        holder.amount.text = "RM ${account.accountAmount.toString()}"
+
+        //Icon
+        val loader = IconPackLoader(holder.icon.context)
+        val iconPack = createDefaultIconPack(loader)
+        val drawable = iconPack.getIconDrawable(account.accountIcon!!, IconDrawableLoader(holder.icon.context))
+        holder.icon.setImageDrawable(drawable)
+        //Change icon to white color
+        holder.icon.setColorFilter(WHITE)
+
+        //Color of account
+        holder.iconBg.setColorFilter(account.accountColor!!)
+
+    }
+
+    override fun getItemCount(): Int {
+        return accountList.size
+    }
+
+    class ListViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView){
+        val title : TextView = itemView.findViewById(R.id.accountNameList_text)
+        val amount : TextView = itemView.findViewById(R.id.accountAmountList_text)
+        val icon : ImageView = itemView.findViewById(R.id.accountIconList_img)
+        val iconBg : ImageView = itemView.findViewById(R.id.accountIconBgList_img)
+    }
 }
