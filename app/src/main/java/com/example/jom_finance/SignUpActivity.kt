f3b43f4ec930ea7c992de10f945e4ab7b986bc25
cheah wooi kang <cheahwooikang02@gmail.com>
2021-12-02 14:23:51 +0800

    commit

--- app/src/main/java/com/example/jom_finance/SignUpActivity.kt
+++ app/src/main/java/com/example/jom_finance/SignUpActivity.kt
@@ -1,11 +1,13 @@
 package com.example.jom_finance
 
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.Toast
+import androidx.preference.PreferenceManager
 import com.example.jom_finance.models.Category
 import com.example.jom_finance.models.User
 import com.google.android.gms.auth.api.signin.GoogleSignIn
@@ -35,8 +37,8 @@ class SignUpActivity : AppCompatActivity(){
         setContentView(R.layout.activity_sign_up)
         setupDataBase()
         btn_Sign_Google.setOnClickListener{
-            //googleSetup()
-            //googleSignIn()
+            googleSetup()
+            googleSignIn()
         }
 
         /*https://android--code.blogspot.com/2020/02/android-kotlin-ktx-clickablespan-example.html for the T&C spanable*/
@@ -220,58 +222,87 @@ class SignUpActivity : AppCompatActivity(){
 
     private fun updateUI(user: Any?) {
         if(user!=null){
-            Toast.makeText(this,"Login Successful", Toast.LENGTH_SHORT).show()
+            //Toast.makeText(this,"Login Successful", Toast.LENGTH_SHORT).show()
             val currentUser = fAuth.currentUser
             if (currentUser != null) {
                 userID = currentUser.uid
             }
+            // If the user is found the Database
             var documentReference = fStore.collection("users").document(userID)
-            val userInfo = User(currentUser?.email.toString(),currentUser?.displayName)
-            documentReference.set(userInfo).addOnSuccessListener {
-                //Name,icon,Color
-                //food,transport,shopping
-                documentReference = fStore.collection("transaction").document(userID)
-                var initialData = HashMap<String,Int>()
-                initialData.put("Income",0)
-                initialData.put("Expense",0)
-                initialData.put("Transaction_counter",0)
-                documentReference.set(initialData).addOnFailureListener{
-                    Toast.makeText(this,it.message.toString(), Toast.LENGTH_SHORT).show()
-                }.addOnSuccessListener {
-                    documentReference = fStore.collection("accounts").document(userID)
-                    var initialAccount = HashMap<String,Int>()
-                    initialAccount.put("Total",0)
-                    documentReference.set(initialAccount).addOnFailureListener{
-                        Toast.makeText(this,it.message.toString(), Toast.LENGTH_SHORT).show()
-                    }
-                    documentReference = fStore.collection("budget").document(userID)
-                    var initialbudget = HashMap<String,Int>()
-                    initialbudget.put("Budget_counter",0)
-                    documentReference.set(initialbudget).addOnFailureListener{
-                        Toast.makeText(this,it.message.toString(), Toast.LENGTH_SHORT).show()
-                    }
-
-                    var categoryNameArray = arrayOf("Food", "Transport", "Shopping")
-                    var categoryIconArray = arrayOf(452,384,258)
-                    var categoryColorArray = arrayOf(-278483,-10044566,-12627531)
-                    var defaultCategory : Category
-                    for(i in 0..2){
-                        documentReference = fStore.collection("category/$userID/category_detail").document(categoryNameArray[i])
-                        defaultCategory = Category(categoryNameArray[i],categoryIconArray[i],categoryColorArray[i])
-                        documentReference.set(defaultCategory)
-                    }
-                    /*// TODO : Direct Login
-                    val intent = Intent(this, LoginActivity::class.java)
+            documentReference.get().addOnCompleteListener {
+                if (it.result.exists()) {
+                    val sharedPreferences =
+                        PreferenceManager.getDefaultSharedPreferences(applicationContext)
+                    // Initialize sharedPreferences to edit
+                    val editor: SharedPreferences.Editor = sharedPreferences.edit()
+                    editor.putBoolean("isLogin", true).apply()
+                    val intent = Intent(this, HomeActivity::class.java)
                     startActivity(intent)
                     overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left)
-                    Toast.makeText(this,"User Account Created", Toast.LENGTH_SHORT).show()*/
+                    finishAffinity()
+                } else {
+                    // If the user not found the Database
+                    val userInfo = User(currentUser?.email.toString(), currentUser?.displayName)
+                    documentReference.set(userInfo).addOnSuccessListener {
+                        //Name,icon,Color
+                        //food,transport,shopping
+                        documentReference = fStore.collection("transaction").document(userID)
+                        var initialData = HashMap<String, Int>()
+                        initialData.put("Income", 0)
+                        initialData.put("Expense", 0)
+                        initialData.put("Transaction_counter", 0)
+                        Log.d("initialData", initialData.toString())
+                        documentReference.set(initialData).addOnFailureListener {
+                            Log.d("Signup", it.message.toString())
+                            //Toast.makeText(this,it.message.toString(), Toast.LENGTH_SHORT).show()
+                        }.addOnSuccessListener {
+                            Log.d("Signup", "Signup Complete")
+                            documentReference = fStore.collection("accounts").document(userID)
+                            var initialAccount = HashMap<String, Int>()
+                            initialAccount.put("Total", 0)
+                            documentReference.set(initialAccount).addOnFailureListener {
+                                Log.d("Signup2", it.message.toString())
+                                //Toast.makeText(this,it.message.toString(), Toast.LENGTH_SHORT).show()
+                            }
+                            documentReference = fStore.collection("budget").document(userID)
+                            var initialbudget = HashMap<String, Int>()
+                            initialbudget.put("Budget_counter", 0)
+                            documentReference.set(initialbudget).addOnFailureListener {
+                                Log.d("Signup3", it.message.toString())
+                                //Toast.makeText(this,it.message.toString(), Toast.LENGTH_SHORT).show()
+                            }
+
+                            var categoryNameArray = arrayOf("Food", "Transport", "Shopping")
+                            var categoryIconArray = arrayOf(452, 384, 258)
+                            var categoryColorArray = arrayOf(-278483, -10044566, -12627531)
+                            var defaultCategory: Category
+                            for (i in 0..2) {
+                                documentReference =
+                                    fStore.collection("category/$userID/category_detail")
+                                        .document(categoryNameArray[i])
+                                defaultCategory = Category(categoryNameArray[i],
+                                    categoryIconArray[i],
+                                    categoryColorArray[i])
+                                documentReference.set(defaultCategory)
+                            }
+                            val sharedPreferences =
+                                PreferenceManager.getDefaultSharedPreferences(applicationContext)
+                            // Initialize sharedPreferences to edit
+                            val editor: SharedPreferences.Editor = sharedPreferences.edit()
+                            editor.putBoolean("isLogin", true).apply()
+                            val intent = Intent(this, HomeActivity::class.java)
+                            startActivity(intent)
+                            overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left)
+                            finishAffinity()
+                        }
+                    }
                 }
             }
 
-            val intent = Intent(this, HomeActivity::class.java)
+      /*    val intent = Intent(this, HomeActivity::class.java)
             startActivity(intent)
             overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left)
-            finish()
+            finish()*/
 
         }else{
             Toast.makeText(this,"Something Wrong", Toast.LENGTH_SHORT).show()
